54b1b8acd4549b0178e5cd731b783ee3a3a8a170
fayded <kevin.d.fay@gmail.com>
2023-02-01 10:53:45 -0500

    setting up server files

--- server/index.js
+++ server/index.js
@@ -4,18 +4,8 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.post("/api/countries", (req, res) => {
+app.get("/api/countries", (req, res) => {
   res.json(countries)
-});
-
-app.get("/api/countries", async (req, res, next) => {
-  console.log("'/get countries'");
-  try {
-    const res = await axios.get("https://jsonplaceholder.typicode.com/users");
-    console.log(res);
-  } catch (error) {
-    console.log(error);
-  }
 });
 
 app.listen(PORT, () => {
