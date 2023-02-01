1a6f4219e31b6a708b056a7a1aa08a92103515a7
fayded <kevin.d.fay@gmail.com>
2023-02-01 12:02:52 -0500

    change port

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ app.get("/api/countries", (req, res) => {
   res.json('I am the countries route')
 });
 
-const server = app.listen(process.env.PORT || 3001, () => {
+const server = app.listen(process.env.PORT || 5000, () => {
     const port = server.address().port;
     console.log(`Express is working on port ${port}`);
 });
